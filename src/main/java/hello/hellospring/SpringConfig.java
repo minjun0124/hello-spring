957f31bde2f43aebfee219b93436fb90e66f8e89
mjlee <minjun0124@gmail.com>
2021-04-21 23:47:28 +0900

    Add : spring-data-jpa / Change Membeer Repo (jpa -> spring-data-jpa)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.jpaMemberRepository;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Configuration
 public class SpringConfig {
@@ -21,7 +16,9 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
 */
-    
+
+/* spring-data-jpa 로 변경
+
 // 원래는 persistenceContext 필요하지만 알아서 DI 해준다
 //    @PersistenceContext
     private EntityManager em;
@@ -30,18 +27,27 @@ public class SpringConfig {
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+*/
 
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+/* spring-data-jpa : SpringDataJpaMemberRepository 에서 구현체를 proxy기술을 사용하여
+                     만들고 spring bean에 등록까지 완료함
 
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new jpaMemberRepository(em);
+        return new jpaMemberRepository(memberRepository);
     }
+*/
 }
